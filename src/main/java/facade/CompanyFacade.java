4dc334b9eddcef4e3d653695a46fcc751896f9df
nivby <56995861+nivby@users.noreply.github.com>
2020-03-12 13:44:20 +0200

    Create CompanyFacade.java

---
+++ src/main/java/facade/CompanyFacade.java
@@ -0,0 +1,53 @@
+package facade;
+
+import dao.CompanyDAO;
+import dao.CouponDAO;
+import dbdao.CompanyDBDAO;
+import dbdao.CouponDBDAO;
+import entities.Company;
+import entities.Coupon;
+import exception.AlreadyExistException;
+import exception.NotLoggedInException;
+import pool.ConnectionPool;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class CompanyFacade implements ClientFacade {
+
+
+    private CompanyDAO companyDAO;
+    private CouponDAO couponDAO;
+    private boolean isLoggedIn;
+
+
+    public CompanyFacade() {
+        this.companyDAO = new CompanyDBDAO();
+        this.couponDAO = new CouponDBDAO();
+        this.isLoggedIn = false;
+    }
+
+
+    @Override
+    public boolean logIn(String email, String password) {
+       isLoggedIn = companyDAO.logIn(email,password);
+       return isLoggedIn;}
+
+    public Coupon createCoupon(Coupon coupon) throws NotLoggedInException,AlreadyExistException {
+
+        if (!isLoggedIn){
+            throw new NotLoggedInException("first you need to log-in");
+        }
+        boolean isExistTitle = false;
+        if (!couponDAO.existByTitle(coupon.getTitle())){
+            isExistTitle = true;
+            return couponDAO.create(coupon);
+        }
+        System.out.println("there is a coupon with a same title... please insert a new title name");
+        return null;
+    }
+
+    
+}
